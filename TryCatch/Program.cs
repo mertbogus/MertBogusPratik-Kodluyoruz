0e8c42a83b9e85a7b837365c891ffd7e7fa046ec
mertbogus <bogusmert@gmail.com>
2022-02-06 17:46:30 +0300

    Hata Yönetimi

---
+++ TryCatch/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace TryCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir Sayı Giriniz:");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş olduğunuz Sayı Şudur: " + sayi);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata:"+ex.Message.ToString());
+            }
+            //finally
+            //{
+            //    Console.WriteLine("İşlem Tamamlandı.");
+            //}
+
+            try
+            {
+                //int a = int.Parse(null);
+                //int a = int.Parse("Test");
+                int a = int.Parse("20000000000000");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş Değer Girdiniz.");
+                Console.WriteLine(ex);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Veri Tipi Uygun Değil.");
+                Console.WriteLine(ex);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Çok küçük veya Çok büyük değer girdiniz.");
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("İşlem Başarıyla Tamamlandı.");
+            }
+
+        }
+    }
+}
